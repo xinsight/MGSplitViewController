65a82347748a1b0a732b2a6384e527ef070436e2
Edward Rudd <urkle@outoforder.cc>
2011-03-20 08:59:39 -0400

    Don't advertise effectively private message for displaying the popoover

--- Classes/MGSplitViewController.h
+++ Classes/MGSplitViewController.h
@@ -54,7 +54,6 @@ typedef enum _MGSplitViewDividerStyle {
 - (IBAction)toggleSplitOrientation; // toggles split axis between vertical (left/right; default) and horizontal (top/bottom).
 - (IBAction)toggleMasterBeforeDetail; // toggles position of master view relative to detail view.
 - (IBAction)toggleMasterView; // toggles display of the master view in the current orientation.
-- (IBAction)showMasterPopover:(id)sender; // shows the master view in a popover spawned from the provided barButtonItem, if it's currently hidden.
 - (void)notePopoverDismissed; // should rarely be needed, because you should not change the popover's delegate. If you must, then call this when it's dismissed.
 
 // Conveniences for you, because I care.
